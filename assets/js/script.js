1a4a85056a44a7dc094295a272542bbff62cda15
kaospctqc <stefan.ioan@sys-admin.ro>
2021-11-05 12:01:44 +0000

    Add initial JavaScript main menu with placeholder text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log('test javascript')
+// Let DOM finish before funning any JavaScript
+
+document.addEventListener('DOMContentLoaded', function() {
+    let menuItems = document.getElementsByClassName('menu-item');
+
+    for (let menuItem of menuItems) {
+        menuItem.addEventListener('click', function() {
+            if (this.textContent === "Play") {
+                displayPlay();
+            } else if (this.textContent === "Instructions") {
+                displayInstructions();
+            } else if (this.textContent === "Feedback") {
+                displayFeedback();
+            } else {
+                alert('No ideea what that was');
+            }
+        })
+    }
+
+    // displayPlay();
+});
+
+/**
+ * Main navigation for the Single Page App thingy
+ */
+ function displayPlay() {
+    document.getElementsByTagName('section')[0].innerHTML = '<p>Play</p>';
+};
+
+function displayInstructions() {
+    document.getElementsByTagName('section')[0].innerHTML = '<p>Instructions</p>';
+};
+
+function displayFeedback() {
+    document.getElementsByTagName('section')[0].innerHTML = '<p>Feedback</p>';
+};
