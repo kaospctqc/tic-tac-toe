231a7adeac9f97ed22e90abe6cfc3772a1210267
kaospctqc <stefan.ioan@sys-admin.ro>
2021-11-06 12:57:33 +0000

    Add dynamic game board and placeholder functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,24 @@ document.addEventListener('DOMContentLoaded', function() {
  * Main navigation for the Single Page App thingy
  */
  function displayPlay() {
-    document.getElementsByTagName('section')[0].innerHTML = '<p>Play</p>';
+
+    let gameState = [
+        ['x', 'o', 'o'],
+        ['o', 'x', 'o'],
+        ['o', 'o', 'x'],
+    ];
+    
+    let score = createScoreBoard();
+    let game = createGameBoard(gameState);
+    let settings = createSettings();
+
+    document.getElementsByTagName('section')[0].innerHTML = `
+        ${score}
+        ${game}
+        ${settings}
+    `;
+
+    console.log(createGameBoard(gameState));
 };
 
 function displayInstructions() {
@@ -33,4 +50,73 @@ function displayInstructions() {
 
 function displayFeedback() {
     document.getElementsByTagName('section')[0].innerHTML = '<p>Feedback</p>';
+};
+
+function createScoreBoard() {
+    return  `
+    <div id="score-board">
+        <div id="player">
+            <div id="player-label" class="label">Player</div>
+            <span id="player-score" class="score">0</span>
+        </div>
+        <div id="computer">
+            <div id="computer-label" class="label">Computer</div>
+            <span id="computer-score" class="score">0</span>
+        </div>
+    </div>
+    `;
+}
+
+function createGameBoard(gameState) {
+    let gameBoard = `
+    <div id="game-board">
+        <table>
+            <tbody>
+    `;
+    for (var i = 0; i < 3; i++) {
+        gameBoard += `           <tr>`;
+        for (var j = 0; j < 3; j++) {
+            gameBoard += `
+                    <td>${gameState[i][j].toUpperCase()}</td>`;
+        }
+        gameBoard += `
+                </tr>
+    `;
+    }
+    gameBoard += `
+            </tbody>
+        </table>
+    </div>
+    `;
+
+    return gameBoard;
+};
+
+function createSettings () {
+    return `
+    <div id="settings">
+        <div id="turn-select">
+            <div id="turn-label" class="label">Go first ?</div>
+            <div id="turn-switch">
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+        <button id="game-restart">Restart game</button>
+    </div>
+    `;
+};
+
+function getScoreState() {
+
+};
+
+function getGameState() {
+    let 
+};
+
+function getSettingsState() {
+
 };
